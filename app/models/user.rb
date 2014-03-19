e450d34975aaeb1a6f9b9871f74fd0197f86544d
chischaschos <larin.s931@gmail.com>
2014-03-19 14:41:15 -0600

    Add basic access token creation

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,10 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :events
+
+  before_save :create_access_token
+
+  def create_access_token
+    self.access_token = Devise.friendly_token
+  end
 end
--- spec/api/events_spec.rb
+++ spec/api/events_spec.rb
@@ -3,12 +3,19 @@ require 'spec_helper'
 describe 'Events API', api: true do
 
   context 'GET events' do
-    before { Fabricate.times(2, :event) }
+    context 'when passing a valid authentication token' do
+      before do
+        events = Fabricate.times(2, :event)
+        events.first.owner
+      end
 
-    it 'should retrieve a list of events' do
-      get api_events_path, {}, CONTENT_TYPE: 'application/json'
-      expect(response_json).to have(2).items
-      expect(response).to be_successful
+      it 'should retrieve a list of events' do
+        get api_events_path, {}, CONTENT_TYPE: 'application/json'
+        expect(response_json).to have(2).items
+        expect(response_json[0]).to include('name')
+        expect(response).to be_successful
+      end
+
     end
   end
 end
