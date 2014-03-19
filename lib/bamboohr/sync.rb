07226d3ab50412f6fd580c71f1880782083bb53c
David Padilla <david@crowdint.com>
2014-03-19 13:14:42 -0600

    BambooHR::Sync.client method and test

--- lib/bamboohr/sync.rb
+++ lib/bamboohr/sync.rb
@@ -1,10 +1,15 @@
 module BambooHR
   class Sync
     def self.users
-      client = BambooHR::Client.new
     end
 
     def self.client
+      unless @client
+        @client = Client.new
+        @client.key = Rails.application.secrets.bamboo_api_key
+        @client.subdomain = Rails.application.secrets.bamboo_subdomain
+      end
+      @client
     end
   end
 end
--- spec/lib/bamboohr/sync_spec.rb
+++ spec/lib/bamboohr/sync_spec.rb
@@ -3,8 +3,15 @@ require 'bamboohr/sync'
 
 describe BambooHR::Sync do
   describe :client do
+    before do
+      Rails.application.secrets.bamboo_api_key = "API_KEY"
+      Rails.application.secrets.bamboo_subdomain = "crowdint"
+    end
+
     it "returns the configured BambooHR::Client object" do
-      expect(BambooHR::Sync.client).to eq []
+      expect(BambooHR::Sync.client).to be_a(BambooHR::Client)
+      expect(BambooHR::Sync.client.key).to eq "API_KEY"
+      expect(BambooHR::Sync.client.subdomain).to eq "crowdint"
     end
   end
 
